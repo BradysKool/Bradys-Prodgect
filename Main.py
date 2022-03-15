2c49563135a568d629d755f1690821870400b20b
BradysKool <101609721+BradysKool@users.noreply.github.com>
2022-03-14 18:14:29 -0700

    bradys using git hub

---
+++ Main.py
@@ -0,0 +1,42 @@
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+ 
+ 
+url = "https://www.dell.com/en-us/shop/dell-laptops/sr/laptops/alienware-laptops"
+page = requests.get(url)
+page = page.text
+ 
+soup = BeautifulSoup(page, 'html.parser')
+def findtitles(mainPage):
+    pc_titles = []
+    #names of pc
+    title =print(soup.title.text)
+    mainPage = soup.find_all("article")
+    for articale in mainPage:
+        pc_title=articale.find("h3",class_ ="ps-title")
+        if pc_title == None:
+            break
+        pc_titles.append(pc_title.text)
+    return(pc_titles)
+ 
+#price of pc
+def find_prices(mainPage):
+    computer_prices = []
+    for article in mainPage:
+        price = article.find('div', class_ = 'ps-dell-price ps-simplified')
+        computer_prices.append(price)
+
+    return(computer_prices)
+
+
+
+df = pd.DataFrame(columns=['name', 'price'])
+names = ['Alienware', 'Dell', 'Mac']
+price =  [300,275,250]
+
+df['name'] = names
+df['price'] = price
+
+print(df['name'])
